dc5a8649ac29278ca8722c2e6e99728ff277bc31
olegCtco <olegs.fedkins@gmail.com>
2013-08-22 18:05:09 +0300

    works standard test cases and author added

--- src/main/java/lv/ctco/metacharactersapp/MetacharactersAppEngine.java
+++ src/main/java/lv/ctco/metacharactersapp/MetacharactersAppEngine.java
@@ -10,6 +10,89 @@ package lv.ctco.metacharactersapp;
 public class MetacharactersAppEngine {
 
     public String stripBackslashNewlineSomeCombinations(String s) {
-        return null;
+        if (isEmpty(s)) return s;
+        if (isNotContainsFromBackslashNewlines(s)) return s;
+        if (isOnlyOneBackslashOrNewLine(s)) return s;
+//        if (isWrongOrder(s)) return s;
+
+        int counter = 0;
+        char[] chars = s.toCharArray();
+        char[] result = new char[s.length()];
+        for (int i = chars.length - 1, y = 0; i >= 0; i--) {
+            if (counter == 0) {
+                if (chars[i] != '\n') {
+                    result[y] = chars[i];
+                    y++;
+                } else if (i == 0) {
+                    result[y] = chars[i];
+                    y++;
+                } else {
+                    counter++;
+                }
+            } else if (counter == 1) {
+                if (chars[i] == '\\') {
+                    counter++;
+                } else {
+                    result[y] = '\n';
+                    y++;
+                    result[y] = chars[i];
+                    y++;
+                    counter = 0;
+                }
+            } else {
+                if (chars[i] == '\\') {
+                    counter++;
+                } else {
+                    result[y] = chars[i];
+                    y++;
+                    counter = 0;
+                }
+            }
+        }
+
+        String beforeReverse = new String(result).trim();
+        String reverse = new StringBuffer(beforeReverse).reverse().toString();
+        return reverse;
     }
+
+    public boolean isEmpty(String s) {
+        if (s == null || s.isEmpty()) return true;
+        return false;
+    }
+
+    public boolean isNotContainsFromBackslashNewlines(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '\\' || s.charAt(i) == '\n') return false;
+        }
+        return true;
+    }
+
+    public boolean isOnlyOneBackslashOrNewLine(String s) {
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '\\' || s.charAt(i) == '\n') {
+                counter++;
+            }
+        }
+        if (counter == 1) return true;
+        return false;
+    }
+
+//    public boolean isWrongOrder(String s) {
+//        boolean flag = true;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (flag) {
+//                if (s.charAt(i) == '\n') {
+//                    flag = false;
+//                }
+//            } else if (!flag) {
+//                if (s.charAt(i) == '\\') {
+//                    return true;
+//                } else {
+//                    flag = true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
--- src/test/java/lv/ctco/metacharactersapp/MetacharactersAppEngineTest.java
+++ src/test/java/lv/ctco/metacharactersapp/MetacharactersAppEngineTest.java
@@ -2,7 +2,7 @@ package lv.ctco.metacharactersapp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -71,4 +71,121 @@ public class MetacharactersAppEngineTest {
         String resultLine = metacharactersAppEngine.stripBackslashNewlineSomeCombinations(inputLine);
         assertEquals(expectedLine, resultLine);
     }
+
+    @Test
+    public void shouldReturnTrueIfNull_isEmpty() {
+        String inputLine = null;
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isEmpty(inputLine);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnTrueIfEmpty_isEmpty() {
+        String inputLine = "";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isEmpty(inputLine);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseIfNotEmpty_isEmpty() {
+        String inputLine = " ";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isEmpty(inputLine);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldTrueBecauseNoRequired_isNotContainsFromBackslashNewlines() {
+        String inputLine = "abcdef";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isNotContainsFromBackslashNewlines(inputLine);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldFalseBecauseBackslash_isNotContainsFromBackslashNewlines() {
+        String inputLine = "abcdef\\";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isNotContainsFromBackslashNewlines(inputLine);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldFalseBecauseNewLine_isNotContainsFromBackslashNewlines() {
+        String inputLine = "abcdef\n";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isNotContainsFromBackslashNewlines(inputLine);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldTrueBecauseOneBackslash_isOnlyOneBackslashOrNewLine() {
+        String inputLine = "abcdef\\";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isOnlyOneBackslashOrNewLine(inputLine);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldTrueBecauseOneNewline_isOnlyOneBackslashOrNewLine() {
+        String inputLine = "abcdef\n";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isOnlyOneBackslashOrNewLine(inputLine);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldFalseBecauseTwoNewlines_isOnlyOneBackslashOrNewLine() {
+        String inputLine = "abcdef\n\n";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isOnlyOneBackslashOrNewLine(inputLine);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldFalseBecauseTwoBackslashes_isOnlyOneBackslashOrNewLine() {
+        String inputLine = "abcdef\\\\";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isOnlyOneBackslashOrNewLine(inputLine);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldFalseBecauseNoBackslashesOrNewlines_isOnlyOneBackslashOrNewLine() {
+        String inputLine = "abcdef";
+
+        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+        boolean result = metacharactersAppEngine.isOnlyOneBackslashOrNewLine(inputLine);
+        assertFalse(result);
+    }
+
+//    @Test
+//    public void shouldTrueBecauseWrongOrder_isWrongOrder(){
+//        String inputLine = "abc\n\\def";
+//
+//        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+//        boolean result = metacharactersAppEngine.isWrongOrder(inputLine);
+//        assertTrue(result);
+//    }
+//
+//    @Test
+//    public void shouldFalseBecauseCorrectOrder_isWrongOrder(){
+//        String inputLine = "abc\\\ndef";
+//
+//        MetacharactersAppEngine metacharactersAppEngine = new MetacharactersAppEngine();
+//        boolean result = metacharactersAppEngine.isWrongOrder(inputLine);
+//        assertFalse(result);
+//    }
 }
