6720f38b3ad92f2bf1a99ae79b1eb9544f81f17e
olegCtco <olegs.fedkins@gmail.com>
2013-08-23 08:58:59 +0300

    regexpresion edition with dynamic backslashes

--- src/main/java/lv/ctco/metacharactersapp/Engine.java
+++ src/main/java/lv/ctco/metacharactersapp/Engine.java
@@ -1,18 +1,22 @@
 package lv.ctco.metacharactersapp;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Engine {
 
     public String stripBackslashNewlineSomeCombinations(String inputLine) {
+        List<String> regexps = new ArrayList<>();
         String regex = "\\\\\n";
-        if (isEmpty(inputLine)) return inputLine;
+        String result = inputLine;
+        regexps.add(regex);
+        for (int i = 0; i < inputLine.length() - 2; i++) {
+            regex = "\\\\" + regex;
+            regexps.add(regex);
+        }
 
-        List<String> stringList = Arrays.asList(inputLine.split(regex));
-        String result = "";
-        for (String s : stringList) {
-            result += s;
+        for (int i = regexps.size() - 1; i >= 0; i--) {
+            result = result.replaceAll(regexps.get(i), "");
         }
         return result;
     }
